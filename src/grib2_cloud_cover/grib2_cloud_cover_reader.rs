ca7b5d114026eaf8c910841350aff849091627b9
opacopac <armand@tschanz.com>
2022-04-15 23:48:45 +0200

    cloud cover layer

--- src/grib2_cloud_cover/grib2_cloud_cover_reader.rs
+++ src/grib2_cloud_cover/grib2_cloud_cover_reader.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::fs::File;
+use std::path::Path;
 
 use crate::grib2_cloud_cover::grib2_cloud_cover_layer::Grib2CloudCoverLayer;
 
@@ -7,6 +9,8 @@ pub struct Grib2CloudCoverReader;
 
 impl Grib2CloudCoverReader {
     pub fn read_file(filename: &str) -> Result<Grib2CloudCoverLayer, Box<dyn Error>> {
+        let file = File::open(filename)?;
+
         return Ok(Grib2CloudCoverLayer {});
     }
 }
