ded3f60e50c117e6ff743429353d6b7a9897eddc
opacopac <armand@tschanz.com>
2022-04-18 21:32:14 +0200

    section 8

--- src/grib2_section8/section8.rs
+++ src/grib2_section8/section8.rs
@@ -1,12 +1,20 @@
 use std::error::Error;
 
+use simple_error::bail;
+
 pub struct Section8 {
     pub end_magic: String
 }
 
 
+const GRIB2_END_MAGIC: &str = "7777";
+
 impl Section8 {
     pub fn new(end_magic: String) -> Result<Section8, Box<dyn Error>> {
+        if end_magic != GRIB2_END_MAGIC {
+            bail!("Invalid end section");
+        }
+
         return Ok(Section8 { end_magic });
     }
 }
