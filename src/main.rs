4b89d91c7d0534ee116011f110c1f767a7c7042d
opacopac <armand@tschanz.com>
2022-04-19 21:28:05 +0200

    refactoring integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-/*use crate::grib2_cloud_cover::cloud_cover_reader::CloudCoverReader;
+use meteo_grib2_renderer::grib2_cloud_cover::cloud_cover_reader::CloudCoverReader;
 
-const CLCT_TEST_FILE: &str = "./src/tests/data/icon-d2_germany_regular-lat-lon_single-level_2022041700_000_2d_clct_mod.grib2";
-
-mod grib2_cloud_cover;*/
+const CLCT_TEST_FILE: &str = "./tests/data/icon-d2_germany_regular-lat-lon_single-level_2022041700_000_2d_clct_mod.grib2";
 
 fn main() {
     println!("Hello, world!");
-    //let _layer = CloudCoverReader::read_file(CLCT_TEST_FILE).unwrap();
+    let _layer = CloudCoverReader::read_file(CLCT_TEST_FILE).unwrap();
 }
