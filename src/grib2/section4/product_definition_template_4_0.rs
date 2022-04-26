4575044b3fd35947c67f4c5289a0cc3dd3a3322e
opacopac <armand@tschanz.com>
2022-04-26 20:50:17 +0200

    dwd precip layer

--- src/grib2/section4/product_definition_template_4_0.rs
+++ src/grib2/section4/product_definition_template_4_0.rs
@@ -1,25 +1,13 @@
 use derive_more::Constructor;
 
-use crate::grib2::common::scale_factor_value::ScaleFactorValue;
 use crate::grib2::section4::meteo_parameter_category::MeteoParameterCategory;
 
 #[derive(Debug, Constructor)]
 pub struct ProductDefinitionTemplate4_0 {
     pub parameter_category: MeteoParameterCategory,
     pub parameter_number: u8,
-    pub generating_process_type: u8,
-    pub generating_process_identifier: u8,
-    pub generating_process: u8,
-    pub hours_cutoff: u16,
-    pub mins_cutoff: u8,
-    pub forecast_time_unit: u8,
-    pub forecast_time_value: u8,
-    pub fixed_surface1_type: u8,
-    pub fixed_surface1_scale_factor_value: ScaleFactorValue,
-    pub fixed_surface2_type: u8,
-    pub fixed_surface2_scale_factor_value: ScaleFactorValue,
 }
 
 impl ProductDefinitionTemplate4_0 {
-    pub const TPL_LENGTH_BYTES: u32 = 22;
+    pub const TPL_LENGTH_BYTES: u32 = 2;
 }
--- src/grib2/section4/section4_template_4_0_reader.rs
+++ src/grib2/section4/section4_template_4_0_reader.rs
@@ -1,11 +1,9 @@
 use std::fs::File;
 use std::io::BufReader;
 
-use byteorder::{BigEndian, ReadBytesExt};
+use byteorder::ReadBytesExt;
 
 use crate::grib2::common::grib2_error::Grib2Error;
-use crate::grib2::common::scale_factor_value::ScaleFactorValue;
-use crate::grib2::common::scale_factor_value_reader::ScaleFactorValueReader;
 use crate::grib2::section4::meteo_parameter_category::MeteoParameterCategory;
 use crate::grib2::section4::product_definition_template_4_0::ProductDefinitionTemplate4_0;
 
@@ -16,33 +14,10 @@ impl Section4Template4_0Reader {
     pub fn read(reader: &mut BufReader<File>) -> Result<ProductDefinitionTemplate4_0, Grib2Error> {
         let parameter_category = Section4Template4_0Reader::read_parameter_category(reader)?;
         let parameter_number = reader.read_u8()?;
-        let generating_process_type = reader.read_u8()?;
-        let generating_process_identifier = reader.read_u8()?;
-        let generating_process = reader.read_u8()?;
-        let hours_cutoff = reader.read_u16::<BigEndian>()?;
-        let mins_cutoff = reader.read_u8()?;
-        let forecast_time_unit = reader.read_u8()?;
-        let forecast_time_value = reader.read_u8()?;
-        let fixed_surface1_type = reader.read_u8()?;
-        let fixed_surface1_scale_factor_value = ScaleFactorValueReader::read(reader)?;
-        let fixed_surface2_type = reader.read_u8()?;
-        let fixed_surface2_scale_factor_value: ScaleFactorValue = ScaleFactorValueReader::read(reader)?;
-
 
         let tpl_4_0 = ProductDefinitionTemplate4_0::new(
             parameter_category,
             parameter_number,
-            generating_process_type,
-            generating_process_identifier,
-            generating_process,
-            hours_cutoff,
-            mins_cutoff,
-            forecast_time_unit,
-            forecast_time_value,
-            fixed_surface1_type,
-            fixed_surface1_scale_factor_value,
-            fixed_surface2_type,
-            fixed_surface2_scale_factor_value
         );
 
         return Ok(tpl_4_0);
