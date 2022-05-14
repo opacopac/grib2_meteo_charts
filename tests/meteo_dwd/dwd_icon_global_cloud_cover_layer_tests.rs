04ff8c4a6a1e5b72a7a90ae3053401bc20d1a92e
opacopac <armand@tschanz.com>
2022-05-14 15:09:59 +0200

    global icon files

--- tests/meteo_dwd/dwd_icon_global_cloud_cover_layer_tests.rs
+++ tests/meteo_dwd/dwd_icon_global_cloud_cover_layer_tests.rs
@@ -1,4 +1,5 @@
 use meteo_grib2_renderer::grib2::document::grib2_document_reader::Grib2DocumentReader;
+use meteo_grib2_renderer::grib2::section3::grid_definition_template::GridDefinitionTemplate;
 use meteo_grib2_renderer::grib2::section4::meteo_parameter_category::MeteoParameterCategory;
 use meteo_grib2_renderer::meteo_dwd::dwd_icon_global_tot_cloud_cover_layer::DwdIconGlobalTotalCloudCoverLayer;
 
@@ -8,6 +9,14 @@ pub const CLCT_TEST_FILE: &str = "./tests/data/icon_global_icosahedral_single-le
 #[test]
 fn it_successfully_reads_an_icon_global_clct_test_file() {
     let doc = Grib2DocumentReader::read_file(CLCT_TEST_FILE).unwrap();
+
+    match &doc.section3.grid_definition_template {
+        GridDefinitionTemplate::UnstructuredGrid(tpl) => {
+            assert_eq!(26, tpl.number_of_grid);
+            assert_eq!(1, tpl.number_of_grid_in_ref);
+        },
+        _ => panic!("invalid grid definition template: {:?}", doc.section3.grid_definition_template)
+    }
 
     let result = DwdIconGlobalTotalCloudCoverLayer::from_grib2(doc);
     assert!(result.is_ok());
