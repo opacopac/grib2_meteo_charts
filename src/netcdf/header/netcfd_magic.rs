301423fdd25a43a0157d9cec28c03a9a49203bbc
opacopac <armand@tschanz.com>
2022-05-14 17:03:08 +0200

    parsing netcdf

--- src/netcdf/header/netcfd_magic.rs
+++ src/netcdf/header/netcfd_magic.rs
@@ -18,9 +18,34 @@ impl NetCdfMagic {
             ));
         }
 
+        if version != 2 {
+            return Err(NetCdfError::InvalidData(
+                format!("Unsupported version {}, expected: {}", version, 2)
+            ));
+        }
+
         return Ok(NetCdfMagic {
             magic,
             version
         });
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::netcdf::header::netcfd_magic::{NETCDF_MAGIC, NetCdfMagic};
+
+    #[test]
+    fn it_verifies_the_correct_magic() {
+        let result = NetCdfMagic::new("TODO".to_string(), 2);
+        assert!(result.is_err());
+    }
+
+
+    #[test]
+    fn it_verifies_the_supported_version() {
+        let result = NetCdfMagic::new(NETCDF_MAGIC.to_string(), 3);
+        assert!(result.is_err());
     }
 }
