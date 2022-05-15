c69dcfca7f671f9af1202f01d27fda1f2132c2eb
opacopac <armand@tschanz.com>
2022-05-15 15:18:48 +0200

    parsing netcdf

--- tests/grib2/netcdf_document_reader_tests.rs
+++ tests/grib2/netcdf_document_reader_tests.rs
@@ -1,6 +1,9 @@
 use meteo_grib2_renderer::netcdf::document::netcdf_document_reader::NetCdfDocumentReader;
 
 pub const NETCDF_ICON_GRID_TEST_FILE: &str = "./tests/data/icon_grid_0009_R02B03_R.nc";
+pub const NOT_FOUND_TEST_FILE: &str = "./tests/data/notfound.nc";
+pub const NON_NETCDF_TEST_FILE: &str = "./tests/data/not_a_grib2_file.grib2";
+
 
 #[test]
 fn it_reads_an_existing_grib2_file() {
@@ -10,22 +13,17 @@ fn it_reads_an_existing_grib2_file() {
 }
 
 
-/*#[test]
+#[test]
 fn it_returns_an_error_if_the_file_doesnt_exist() {
-    let grib2_file =  DATA_DIR.to_string() + "notfound.grib2";
+    let result = NetCdfDocumentReader::read_file(NOT_FOUND_TEST_FILE);
 
-    let result = Grib2DocumentReader::read_file(&grib2_file);
-
-    assert_eq!(true, result.is_err());
+    assert!(result.is_err());
 }
 
 
 #[test]
 fn it_returns_an_error_if_the_file_isnt_in_grib2_format() {
-    let grib2_file = DATA_DIR.to_string() + "not_a_grib2_file.grib2";
+    let result = NetCdfDocumentReader::read_file(NON_NETCDF_TEST_FILE);
 
-    let result = Grib2DocumentReader::read_file(&grib2_file);
-
-    assert_eq!(true, result.is_err());
+    assert!(result.is_err());
 }
-*/
