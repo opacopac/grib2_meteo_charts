3d16315fe32d65519719bcc67906305d3a008539
opacopac <armand@tschanz.com>
2022-05-15 12:02:21 +0200

    parsing netcdf

--- src/netcdf/header/netcdf_header.rs
+++ src/netcdf/header/netcdf_header.rs
@@ -1,9 +1,15 @@
 use derive_new::new;
 
+use crate::netcdf::header::netcdf_attr::NetCdfAttr;
+use crate::netcdf::header::netcdf_dim_list::NetCdfDimList;
 use crate::netcdf::header::netcdf_magic::NetCdfMagic;
+use crate::netcdf::header::netcdf_var::NetCdfVar;
 
 #[derive(new)]
 pub struct NetCdfHeader {
     pub magic: NetCdfMagic,
-    pub num_recs: u32
+    pub num_recs: u32,
+    pub dim_list: NetCdfDimList,
+    pub att_list: Vec<NetCdfAttr>,
+    pub var_list: Vec<NetCdfVar>
 }
--- src/netcdf/header/netcdf_header_reader.rs
+++ src/netcdf/header/netcdf_header_reader.rs
@@ -1,9 +1,13 @@
-use std::io::{BufReader, Read, Seek};
+use std::io::{BufReader, Cursor, Read, Seek};
+
 use byteorder::{BigEndian, ReadBytesExt};
 
 use crate::netcdf::common::netcdf_error::NetCdfError;
+use crate::netcdf::header::netcdf_attr_list_reader::NetCdfAttrListReader;
+use crate::netcdf::header::netcdf_dim_list_reader::NetCdfDimListReader;
 use crate::netcdf::header::netcdf_header::NetCdfHeader;
 use crate::netcdf::header::netcdf_magic_reader::NetCdfMagicReader;
+use crate::netcdf::header::netcdf_var_list_reader::NetCdfVarListReader;
 
 pub struct NetCdfHeaderReader;
 
@@ -12,10 +16,16 @@ impl NetCdfHeaderReader {
     pub fn read<T: Read + Seek>(reader: &mut BufReader<T>) -> Result<NetCdfHeader, NetCdfError> {
         let magic = NetCdfMagicReader::read(reader)?;
         let num_recs = reader.read_u32::<BigEndian>()?;
+        let dim_list = NetCdfDimListReader::read(reader)?;
+        let att_list = NetCdfAttrListReader::read(reader)?;
+        let var_list = NetCdfVarListReader::read(reader)?;
 
         let header = NetCdfHeader::new(
             magic,
-            num_recs
+            num_recs,
+            dim_list,
+            att_list,
+            var_list
         );
 
         return Ok(header);
@@ -23,25 +33,22 @@ impl NetCdfHeaderReader {
 }
 
 
-#[cfg(test)]
-mod tests {
-    use std::io::{BufReader, Cursor};
+#[test]
+fn it_correctly_parses_a_header_of_an_file_with_empty_lists() {
+    let mut reader = BufReader::new(Cursor::new([
+        0x43, 0x44, 0x46, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00
+    ]));
 
-    use crate::netcdf::header::netcdf_header_reader::NetCdfHeaderReader;
+    let result = NetCdfHeaderReader::read(&mut reader);
+    assert!(result.is_ok());
 
-    #[test]
-    fn it_correctly_parses_the_header() {
-        let mut reader = BufReader::new(Cursor::new([
-            0x43, 0x44, 0x46, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x00, 0x00, 0x00, 0x0E,
-            0x00, 0x00, 0x00, 0x04, 0x63, 0x65, 0x6C, 0x6C, 0x00, 0x2D, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06
-        ]));
+    let header = result.unwrap();
+    assert_eq!("CDF".to_string(), header.magic.magic);
+    assert_eq!(2, header.magic.version);
+    assert_eq!(0, header.dim_list.dims.len());
+    assert_eq!(0, header.att_list.len());
+    assert_eq!(0, header.var_list.len());
 
-        let result = NetCdfHeaderReader::read(&mut reader);
-        assert!(result.is_ok());
-
-        let header = result.unwrap();
-        assert_eq!("CDF", header.magic.magic);
-        assert_eq!(2, header.magic.version);
-        assert_eq!(0, header.num_recs);
-    }
+    assert_eq!(20 as u64, reader.stream_position().unwrap())
 }
