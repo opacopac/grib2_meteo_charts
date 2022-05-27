e7ba325b8219533e39f97b99e581a3506e0df460
opacopac <armand@tschanz.com>
2022-05-27 22:31:51 +0200

    refactoring

--- src/chart/mod.rs
+++ src/chart/mod.rs
@@ -1,5 +1,3 @@
-pub mod map_tile_renderer;
-pub mod single_chart_renderer;
 pub mod wind_chart_renderer;
 pub mod precip_chart_renderer;
 pub mod wind_arrow_service;
--- src/geo/mod.rs
+++ src/geo/mod.rs
@@ -2,5 +2,4 @@ pub mod map_tile_coord;
 pub mod lat_lon;
 pub mod lat_lon_extent;
 pub mod lat_lon_grid;
-pub mod unstructured_grid;
 pub mod map_tile_grid;
