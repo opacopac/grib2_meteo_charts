7a33a9314abc030def7bb8ee2bec9308d270dde0
ue55617 <armand.tschanz@sbb.ch>
2025-08-08 18:10:28 +0200

    adding support for grib2 clon/clat files for icon-ch1

--- src/grid/unstructured_grid.rs
+++ src/grid/unstructured_grid.rs
@@ -1,40 +1,87 @@
 use crate::geo::lat_lon::LatLon;
+use crate::geo::lat_lon_extent::LatLonExtent;
 
 pub struct UnstructuredGrid {
     dimensions: (usize, usize),
     coordinates: Vec<LatLon>,
+    lat_lon_extent: LatLonExtent,
 }
 
 impl UnstructuredGrid {
-    pub fn new(dimensions: (usize, usize), coordinates: Vec<LatLon>) -> UnstructuredGrid {
+    pub fn new(
+        dimensions: (usize, usize),
+        coordinates: Vec<LatLon>,
+        lat_lon_extent: LatLonExtent,
+    ) -> UnstructuredGrid {
         UnstructuredGrid {
             dimensions,
             coordinates,
+            lat_lon_extent,
         }
+    }
+
+    pub fn new_with_mbr(dimensions: (usize, usize), coordinates: Vec<LatLon>) -> UnstructuredGrid {
+        let lat_lon_extent = UnstructuredGrid::calc_min_bounding_extent(&coordinates);
+        UnstructuredGrid {
+            dimensions,
+            coordinates,
+            lat_lon_extent,
+        }
+    }
+
+    pub fn calc_min_bounding_extent(coordinates: &Vec<LatLon>) -> LatLonExtent {
+        let mut min_lat = LatLon::MIN_LAT;
+        let mut max_lat = LatLon::MAX_LAT;
+        let mut min_lon = LatLon::MIN_LON;
+        let mut max_lon = LatLon::MAX_LON;
+
+        for coord in coordinates {
+            if coord.lat < min_lat {
+                min_lat = coord.lat;
+            }
+            if coord.lat > max_lat {
+                max_lat = coord.lat;
+            }
+            if coord.lon < min_lon {
+                min_lon = coord.lon;
+            }
+            if coord.lon > max_lon {
+                max_lon = coord.lon;
+            }
+        }
+
+        LatLonExtent::new(LatLon::new(min_lat, min_lon), LatLon::new(max_lat, max_lon))
     }
 }
 
-/*
 #[cfg(test)]
 mod tests {
-    use crate::geo::lat_lon::LatLon;
-    use crate::geo::lat_lon_extent::LatLonExtent;
-    use crate::grid::lat_lon_grid::LatLonGrid;
-    use assert_approx_eq::assert_approx_eq;
-
-    fn create_test_grid() -> LatLonGrid {
+    /*fn create_test_grid() -> LatLonGrid {
         let dimensions = (2, 3);
         let lat_lon_extent = LatLonExtent::new(LatLon::new(40.0, 7.0), LatLon::new(46.0, 9.0));
 
         return LatLonGrid::new(dimensions, lat_lon_extent);
-    }
+    }*/
+    use crate::geo::lat_lon::LatLon;
+    use crate::geo::lat_lon_extent::LatLonExtent;
 
     #[test]
-    fn it_creates_a_new_instance_with_the_correct_lat_lon_incs() {
-        let grid = create_test_grid();
+    fn it_creates_a_new_instance() {
+        // given
+        let dimensions = (2, 3);
+        let coordinates = vec![
+            LatLon::new(40.0, 7.0),
+            LatLon::new(41.0, 7.0),
+            LatLon::new(42.0, 7.0),
+        ];
+        let lat_lon_extent = LatLonExtent::MAX_EXTENT;
 
-        assert_eq!(2.0, grid.lat_inc);
-        assert_eq!(1.0, grid.lon_inc);
+        // when
+        let grid = super::UnstructuredGrid::new(dimensions, coordinates, lat_lon_extent);
+
+        // then
+        assert_eq!((2,3), grid.dimensions);
+        assert_eq!(3, grid.coordinates.len());
+        assert_eq!(LatLonExtent::MAX_EXTENT, grid.lat_lon_extent);
     }
 }
-*/
