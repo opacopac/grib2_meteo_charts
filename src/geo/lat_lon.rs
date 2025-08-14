0c309c4bc140e7be3317b3e057742b26c2b91cee
opacopac <armand@tschanz.com>
2025-08-14 21:52:13 +0200

    adding support for icon-ch1 data

--- src/geo/lat_lon.rs
+++ src/geo/lat_lon.rs
@@ -1,9 +1,8 @@
 #[derive(Debug)]
 pub struct LatLon {
     pub lat: f32,
-    pub lon: f32
+    pub lon: f32,
 }
-
 
 impl LatLon {
     pub const MIN_LAT: f32 = -90.0;
@@ -11,44 +10,37 @@ impl LatLon {
     pub const MIN_LON: f32 = -180.0;
     pub const MAX_LON: f32 = 180.0;
 
-    pub const MIN_COORD: LatLon = LatLon { lat: Self::MIN_LAT, lon: Self::MIN_LON };
-    pub const MAX_COORD: LatLon = LatLon { lat: Self::MAX_LAT, lon: Self::MAX_LON };
+    pub const MIN_COORD: LatLon = LatLon {
+        lat: Self::MIN_LAT,
+        lon: Self::MIN_LON,
+    };
+    pub const MAX_COORD: LatLon = LatLon {
+        lat: Self::MAX_LAT,
+        lon: Self::MAX_LON,
+    };
 
-
-    // TODO
-    pub fn new(
-        mut lat: f32,
-        mut lon: f32
-    ) -> LatLon {
-        if lat >= 360.0 || lat < -180.0 {
-            lat = lat.rem_euclid(360.0);
-        }
-        if lon >= 360.0 || lon < Self::MIN_LON {
-            lon = lon.rem_euclid(360.0);
-        }
-        if lat >= 180.0 {
-            lat = lat - 360.0;
-        }
-        if lon >= Self::MAX_LON {
-            lon = lon - 360.0;
+    pub fn new(lat: f32, lon: f32) -> LatLon {
+        if lat < Self::MIN_LAT
+            || lat > Self::MAX_LAT
+            || lon < Self::MIN_LON
+            || lon > Self::MAX_LON
+        {
+            panic!("lat/lon values out of bounds: lat: {}, lon: {}", lat, lon);
         }
 
-        return LatLon {
-            lat,
-            lon
-        }
+        LatLon { lat, lon }
     }
-
 
     pub fn clone(&self) -> LatLon {
-        return LatLon { lat: self.lat, lon: self.lon };
+        LatLon {
+            lat: self.lat,
+            lon: self.lon,
+        }
     }
-
 
     pub fn as_array(&self) -> [f32; 2] {
-        return [ self.lat, self.lon ];
+        [self.lat, self.lon]
     }
-
 
     pub fn calc_euclidean_dist_squared(&self, other: &LatLon) -> f32 {
         let lat_diff = self.lat - other.lat;
@@ -59,35 +51,34 @@ impl LatLon {
     }
 }
 
-
 impl PartialEq for LatLon {
     fn eq(&self, other: &Self) -> bool {
         return self.lat == other.lat && self.lon == other.lon;
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use crate::geo::lat_lon::LatLon;
 
-
-    // TODO: lat: -90 - 90
     #[test]
-    fn it_always_creates_angle_values_between_m180_and_p180() {
+    fn it_always_creates_an_instance() {
         let result1 = LatLon::new(60.5, -40.1);
         assert_eq!(result1.lat, 60.5);
         assert_eq!(result1.lon, -40.1);
-
-        let result2 = LatLon::new(360.5, 270.0);
-        assert_eq!(result2.lat, 0.5);
-        assert_eq!(result2.lon, -90.0);
-
-        let result3 = LatLon::new(360.0, -720.0);
-        assert_eq!(result3.lat, 0.0);
-        assert_eq!(result3.lon, 0.0);
     }
 
+    #[test]
+    #[should_panic]
+    fn it_panics_on_invalid_lat() {
+        LatLon::new(-91.0, 0.0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_panics_on_invalid_lon() {
+        LatLon::new(0.0, 180.5);
+    }
 
     #[test]
     fn it_clones_a_value() {
@@ -97,7 +88,6 @@ mod tests {
 
         assert_eq!(pos, clone);
     }
-
 
     #[test]
     fn it_clones_the_min_max_coords() {
