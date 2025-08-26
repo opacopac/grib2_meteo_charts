1b31f35b74cac5f97e3c0e54172a50002e4edc8a
ue55617 <armand.tschanz@sbb.ch>
2025-08-26 18:14:15 +0200

    adding support for icon-ch1 model

--- src/meteo_swiss/forecast_renderer/icon_ch1_forecast_renderer.rs
+++ src/meteo_swiss/forecast_renderer/icon_ch1_forecast_renderer.rs
@@ -1,6 +1,7 @@
 use crate::meteo_swiss::meteo_swiss_error::MeteoSwissError;
 use log::info;
 use crate::meteo_swiss::forecast_renderer::icon_ch1_forecast_run_finder::IconCh1ForecastRunFinder;
+use crate::meteo_swiss::forecast_renderer::icon_ch1_forecast_urls_finder::IconCh1ForecastUrlsFinder;
 
 pub struct IconCh1ForecastRenderer;
 
@@ -8,9 +9,16 @@ impl IconCh1ForecastRenderer {
     pub fn create_latest_dwd_forecasts() -> Result<(), MeteoSwissError> {
         info!("rendering latest icon ch1 forecasts...");
 
-        info!("search available forecasts...");
+        info!("search latest available forecast...");
         let latest_ref_datetime = IconCh1ForecastRunFinder::find_latest_ref_datetime()?;
         info!("latest ref datetime found: {:?}", latest_ref_datetime);
+
+        info!("search t2m forecast steps...");
+        let forecast_steps_t2m = IconCh1ForecastUrlsFinder::find_forecast_file_urls(
+            crate::meteo_swiss::forecast_run::icon_ch_forecast_variable::IconChForecastVariable::T2m,
+            latest_ref_datetime,
+        );
+        info!("found {} t2m forecast steps", forecast_steps_t2m.as_ref().map(|s| s.len()).unwrap_or(0));
 
         info!("rendering cloud & precipitation forecast...");
         // IconD2CloudPrecipRenderer::create(&latest_run)?;
