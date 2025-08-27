3d112e1809b03124f96618d16132840922f1a4e7
ue55617 <armand.tschanz@sbb.ch>
2025-08-27 14:39:20 +0200

    adding support for icon-ch1 model

--- src/meteo_swiss/forecast_renderer/icon_ch1_temp_forecast_renderer.rs
+++ src/meteo_swiss/forecast_renderer/icon_ch1_temp_forecast_renderer.rs
@@ -1,22 +1,23 @@
-use std::fs;
-use std::fs::File;
-use std::io::{BufWriter, Write};
-
-use log::info;
-use rayon::prelude::{IntoParallelIterator, ParallelIterator};
-
+use crate::chart::temp_chart_renderer::TempChartRenderer;
 use crate::dwd::dwd_file_reader::icon_d2_t_2m_reader::IconD2T2mReader;
 use crate::dwd::dwd_forecast_renderer::forecast_renderer_error::ForecastRendererError;
 use crate::dwd::dwd_forecast_renderer::icon_d2_forecast_renderer_helper::IconD2ForecastRendererHelper;
 use crate::dwd::forecast_run::dwd_forecast_run::DwdForecastRun;
 use crate::dwd::forecast_run::dwd_forecast_step::DwdForecastStep;
+use crate::imaging::drawable::Drawable;
 use crate::meteo_layer::meteo_temp_layer::MeteoTempLayer;
 use crate::metobin::temp_metobin::TempMeteoBin;
+use log::info;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+use std::fs;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use crate::meteo_swiss::forecast_run::icon_ch_forecast_step::IconCh1ForecastStep;
+
+const TEMP_LAYER: &str = "temp";
 
 
 pub struct IconCh1TempForecastRenderer;
-
-const TEMP_LAYER: &str = "temp";
 
 
 impl IconCh1TempForecastRenderer {
@@ -31,11 +32,15 @@ impl IconCh1TempForecastRenderer {
                 let layer = MeteoTempLayer::new(temp_grid)?;
 
                 // map tiles
-                /*let _ = WindChartRenderer::render_map_tiles(
+                let zoom_range = (0, 7);
+                let save_fn = |tile: &Drawable, zoom: u32, x: u32, y: u32| IconD2ForecastRendererHelper::save_tile_step(
+                    tile, zoom, x, y, TEMP_LAYER, &fc_step
+                );
+                let _ = TempChartRenderer::render_map_tiles(
                     &layer,
-                    (0, 7),
-                    |tile: &Drawable, zoom: u32, x: u32, y: u32| IconD2ForecastRendererHelper::save_tile_step(tile, zoom, x, y, WIND_LAYER, &fc_step)
-                );*/
+                    zoom_range,
+                    save_fn,
+                );
 
                 // meteobin
                 let temp_bin = TempMeteoBin::new(layer);
