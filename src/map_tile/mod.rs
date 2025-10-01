a3f681a99419947a1d85f24aa6f9d9124648f2ec
ue55617 <armand.tschanz@sbb.ch>
2025-10-01 15:26:53 +0200

    simplifying map tile renderer

--- src/map_tile/mod.rs
+++ src/map_tile/mod.rs
@@ -1,4 +1,3 @@
 pub mod map_tile_coord;
 pub mod map_tile_renderer;
 pub mod map_tile_error;
-pub mod map_tile_file_helper;
--- src/meteo_chart/forecast_renderer/mod.rs
+++ src/meteo_chart/forecast_renderer/mod.rs
@@ -3,3 +3,4 @@ pub mod wind_chart_renderer;
 pub mod cloud_precip_chart_renderer;
 pub mod temp_chart_renderer;
 pub mod meteo_chart_error;
+pub mod map_tile_file_helper;
