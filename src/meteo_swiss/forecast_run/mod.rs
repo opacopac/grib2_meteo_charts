c56c5d1adc921ac0fc26002fd92ff2cf363cbe47
ue55617 <armand.tschanz@sbb.ch>
2025-11-26 14:34:49 +0100

    refactoring forecast renderer

--- src/meteo_swiss/forecast_run/mod.rs
+++ src/meteo_swiss/forecast_run/mod.rs
@@ -1,7 +1,6 @@
 pub mod icon_ch_forecast_model;
 pub mod icon_ch_forecast_variable;
 pub mod icon_ch_forecast_horizon;
-pub mod icon_ch_forecast_run;
 pub mod icon_ch_forecast_step;
 pub mod icon_ch_forecast_run_name;
 pub mod icon_ch_forecast_reference_datetime;
